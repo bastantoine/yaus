f4c2812e678c7912bee9449d6aea8dbf60dcf5c1
bastantoine <bastien.antoine69+github@gmail.com>
2021-08-31 23:00:21 +0200

    Added endpoint to create link handler

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/md5"
 	"database/sql"
+	"encoding/hex"
 	"flag"
 	"log"
 	"os"
@@ -20,23 +22,37 @@ const (
 )`
 )
 
-func exec(query string) error {
+type Body struct {
+	Url     string `binding:"required"`
+	Handler string
+}
+
+func exec(query string, args ...interface{}) error {
 	db, err := sql.Open("sqlite3", db_filename+open_options)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	_, err = db.Exec(query)
+	_, err = db.Exec(query, args...)
 	return err
 }
 
-func query(query string) (*sql.Rows, error) {
+func query(query string, args ...interface{}) (*sql.Rows, error) {
 	db, err := sql.Open("sqlite3", db_filename+open_options)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	return db.Query(query)
+	return db.Query(query, args...)
+}
+
+func insert_link(link, handler string) (string, error) {
+	if handler == "" {
+		hash := md5.Sum([]byte(link))
+		handler = hex.EncodeToString(hash[:])[:6]
+	}
+	err := exec(`INSERT INTO links (link, handler) VALUES (?, ?)`, link, handler)
+	return handler, err
 }
 
 func main() {
@@ -67,8 +83,18 @@ func main() {
 				panic(err)
 			}
 			c.Redirect(302, link)
+		} else {
+			c.String(404, "No link found for "+hash)
 		}
-		c.String(404, "No link found for "+hash)
+	})
+	r.POST("/link", func(c *gin.Context) {
+		var body Body
+		c.BindJSON(&body)
+		handler, err := insert_link(body.Url, body.Handler)
+		if err != nil {
+			panic(err)
+		}
+		c.JSON(200, gin.H{"handler": handler})
 	})
 	r.NoRoute(func(c *gin.Context) {
 		c.String(404, "Not found")
